f4123223b01ad7ef196423596ef34e1a6552e921
Joan Cuch <joancuch@gmail.com>
2025-06-13 15:38:30 +0200

    added reload button

--- app.py
+++ app.py
@@ -6,39 +6,39 @@ import matplotlib.pyplot as plt
 
 st.title("Capybara Go Simulator")
 
-try:
-    config_df = initialize_config()
+clicked = st.button("Reload Page")
+if clicked: 
+    st.rerun()
+    
+config_df = initialize_config()
 
-    #st.write(config_df)
+#st.write(config_df)
+st.divider()
+st.subheader("Chapter Daily Log")
+chapter_log = model(config_df)
 
-    chapter_log = model(config_df)
+st.divider
 
+st.dataframe(chapter_log.daily_log.log)
 
-    st.dataframe(chapter_log.daily_log.log)
+log_df = chapter_log.daily_log.to_dataframe()
+#st.line_chart(log_df.set_index("day")[["player_atk", "player_def", "player_hp", "player_maxhp"]])
+#st.line_chart(log_df.set_index("day")[["player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"]])
 
-    log_df = chapter_log.daily_log.to_dataframe()
-    #st.line_chart(log_df.set_index("day")[["player_atk", "player_def", "player_hp", "player_maxhp"]])
-    #st.line_chart(log_df.set_index("day")[["player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"]])
+st.line_chart(
+    log_df[["day", "player_atk", "player_def", "player_hp", "player_maxhp"]],
+    x = "day",
+    y = ["player_atk", "player_def", "player_hp", "player_maxhp"],
+    x_label= "day",
+    y_label= "player stats",
+    color=["#000000", "#1982C4", "#FF595E", "#FFCA3A"]
+)
 
-    st.line_chart(
-        log_df[["day", "player_atk", "player_def", "player_hp", "player_maxhp"]],
-        x = "day",
-        y = ["player_atk", "player_def", "player_hp", "player_maxhp"],
-        x_label= "day",
-        y_label= "player stats",
-        color=["#000000", "#1982C4", "#FF595E", "#FFCA3A"]
-    )
-
-    st.line_chart(
-        log_df[["day", "player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"]],
-        x = "day",
-        y = ["player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"],
-        x_label= "day",
-        y_label= "player stats",
-        color=["#000000", "#1982C4", "#FF595E", "#FFCA3A"]
-    )
-
-
-except Exception as e:
-    st.error(f"Some error happened: {e}")
-    st.exception(e)
+st.line_chart(
+    log_df[["day", "player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"]],
+    x = "day",
+    y = ["player_delta_atk", "player_delta_def", "player_delta_hp", "player_delta_maxhp"],
+    x_label= "day",
+    y_label= "player stats",
+    color=["#000000", "#1982C4", "#FF595E", "#FFCA3A"]
+)
